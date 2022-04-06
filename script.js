91f892dbb0d2e93cdfe28eacea27bdcc92af670e
DuAlmeida <99351100+du-almeida@users.noreply.github.com>
2022-04-06 16:24:29 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,16 @@
+function mudaModo(){
+  document.body.classList.toggle('dark')
+}
+
+function typeWriter(elemento) {
+  const textoArray = elemento.innerHTML.split('')
+  elemento.innerHTML = '';
+  textoArray.forEach((letra, i) => {
+    setTimeout(() => elemento.innerHTML += letra, 75 * i)
+  });
+}
+
+
+const titulo = document.querySelector("h1");
+typeWriter(titulo)
+typeWriter(document.querySelector(".texto"));
